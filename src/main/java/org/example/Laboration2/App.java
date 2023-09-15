0f089e9ab68bc22334ecfdc2308da3c41d31b05d
Anton Holst <holst9559@hotmail.com>
2023-09-15 11:40:50 +0200

    Added comparable which I missed and assignment2

--- src/main/java/org/example/Laboration2/App.java
+++ src/main/java/org/example/Laboration2/App.java
@@ -1,22 +1,20 @@
 package org.example.Laboration2;
 
-import java.awt.image.CropImageFilter;
-import java.sql.SQLOutput;
-import java.util.ArrayList;
+import java.util.*;
 
 public class App {
     public static void main(String[] args) {
-        ArrayList<Shape> shapeList = new ArrayList<>();
+        List<Shape> shapeList = new ArrayList<>();
 
-        shapeList.add(new Rectangle(2, 4));
-        shapeList.add(new Rectangle(4, 8));
-        shapeList.add(new Rectangle(8, 12));
-        shapeList.add(new Rectangle(10, 14));
+        shapeList.add(Shape.newRectangle(2, 4));
+        shapeList.add(Shape.newRectangle(4, 8));
+        shapeList.add(Shape.newRectangle(8, 12));
+        shapeList.add(Shape.newRectangle(10, 14));
 
-        shapeList.add(new Circle(2));
-        shapeList.add(new Circle(4));
-        shapeList.add(new Circle(7));
-        shapeList.add(new Circle(10));
+        shapeList.add(Shape.newCircle(2));
+        shapeList.add(Shape.newCircle(4));
+        shapeList.add(Shape.newCircle(7.5f));
+        shapeList.add(Shape.newCircle(10));
 
         System.out.println("Unsorted: " + shapeList);
         shapeList.sort(null);
--- src/main/java/org/example/Laboration2/Shape.java
+++ src/main/java/org/example/Laboration2/Shape.java
@@ -1,8 +1,14 @@
 package org.example.Laboration2;
 
-public abstract class Shape {
+
+public abstract class Shape implements Comparable<Shape>{
     abstract double getArea();
     abstract double getPerimeter();
+
+    @Override
+    public int compareTo(Shape o) {
+        return Double.compare(this.getArea(), o.getArea());
+    }
 
     public static Circle newCircle(float radius){
         return new Circle(radius);
